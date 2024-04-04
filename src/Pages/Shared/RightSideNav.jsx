60f605f320f3e0d3c14f1d41e2af2844d9ffe5fa
Arafat islam <arafat.innotimes.net@gmail.com>
2024-04-04 17:58:30 +0600

    right side navigation Added

--- src/Pages/Shared/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav.jsx
@@ -1,4 +1,7 @@
-import { FaGoogle,FaGithub  } from "react-icons/fa";
+import { FaGoogle,FaGithub, FaFacebook, FaTwitter, FaInstagram  } from "react-icons/fa";
+import qZone1 from "../../assets/qZone1.png"
+import qZone2 from "../../assets/qZone2.png"
+import qZone3 from "../../assets/qZone3.png"
 const RightSideNav = () => {
     return (
         <div>
@@ -12,6 +15,29 @@ const RightSideNav = () => {
                     <FaGithub />
                     log in with github
                 </button>
+            </div>
+            <div className="p-4 space-y-1 mb-6">
+                <h2 className="texl-3xl mb-4"> Find Us on</h2>
+                <a className="flex p-4  text-lg items-center border-2 bg-slate-200 rounded-t-lg" href="">
+                    <FaFacebook className="mr-5" />
+                    Facebook
+                </a>
+                <a className="flex p-4  text-lg items-center  border-2 bg-slate-200 rounded-t-lg" href="">
+                    <FaTwitter className="mr-5"/>
+                    Twitter
+                </a>
+                <a className="flex p-4  text-lg items-center border-2 bg-slate-200 rounded-t-lg" href="">
+                    <FaInstagram className="mr-5"/>
+                    Instragram
+                </a>
+            </div>
+
+            <div className="p-4 space-y-3">
+                <h3 className="text-2xl font-semibold">Q zone</h3>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+                
             </div>
         </div>
     );
